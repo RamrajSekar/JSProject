2ea58d7121dd946e84e707ac42cce8320aa5f952
RamrajSekar <sekar.ramraj@gmail.com>
2020-07-08 15:03:46 +0530

    about variables

--- 01Basics/HelloWorld.js
+++ 01Basics/HelloWorld.js
@@ -1,2 +1,30 @@
 console.log('Hello World..!');
-console.log('I am coming wiht JS..!');
+console.log('I am coming with JS..!');
+
+//string variables
+var firstname = 'Ramraj';
+var lastname = 'Sekar';
+
+// concatenation
+console.log(firstname +' '+ lastname)
+
+// JS is a dynamic typed programming language: data types are assigned automatically
+// primitive data types: 
+// Number,String, Boolean, Undefined[variable without value], Null[non-existent]
+
+var age = 33;
+
+var fullAge = true;
+console.log(fullAge);
+
+// Undefined variable
+var role;
+role = "Software Engineer";
+console.log(role);
+
+// other way to declare variables
+var state, city, married;
+state = "Tamil Nadu";
+city = 'Chennai';
+married = true;
+console.log(married, city, state)
